ffdaacda5e1ef174d8b4f3f808d02284177350e8
AvhiMaz <avhimazumder5@outlook.com>
2024-04-20 09:40:10 +0530

    added founder photo in footer

--- src/app/components/animated.tsx
+++ src/app/components/animated.tsx
@@ -6,8 +6,7 @@ const people = [
     id: 1,
     name: "Priyangshu Das",
     designation: "Founder / Operations Manager",
-    image:
-      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    image: "https://avatars.githubusercontent.com/u/167234039?v=4",
   },
   {
     id: 2,
