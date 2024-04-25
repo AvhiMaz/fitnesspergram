3c851888f40e52db63698ed3eaa488d28a8fd60d
AvhiMaz <avhimazumder5@outlook.com>
2024-04-25 21:58:35 +0530

    copyright size change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
             <NavbarDemo />
             {children}
             <footer className="text-center text-white">
-              <p className="text-[10px]">
+              <p className="text-[15px]">
                 © 2024 fitnesspergram | Email : info@fitnesspergram.store |
                 <br /> Phone : +917086615792 | Business Address : Guwahati
                 <br />
