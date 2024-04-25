4e0594146cf3065d2b36765578df7584b4c3f3ae
AvhiMaz <avhimazumder5@outlook.com>
2024-04-25 21:56:48 +0530

    color change

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -7,7 +7,7 @@ export default function SpotlightPreview() {
     <>
       <section className="bg-black text-gray-100 py-32 min-h-screen">
         <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
-          <h2 className="mb-12 text-4xl font-bold leadi text-center sm:text-5xl">
+          <h2 className="mb-12 text-yellow-500 text-4xl font-bold leadi text-center sm:text-5xl">
             Frequently Asked Questions
           </h2>
           <div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-700">
