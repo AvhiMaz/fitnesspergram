9af0df34e26bb239847041cc23bb186adf306079
AvhiMaz <avhimazumder5@outlook.com>
2024-04-26 11:12:59 +0530

    changes in ui

--- src/app/components/tracing.tsx
+++ src/app/components/tracing.tsx
@@ -56,8 +56,7 @@ const dummyContent = [
       </>
     ),
     badge: "",
-    image:
-      "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "https://iili.io/JUxwSFS.png",
   },
   {
     title: "Our Vision",
@@ -100,8 +99,7 @@ const dummyContent = [
       </>
     ),
     badge: "",
-    image:
-      "https://iili.io/JSyePNs.jpg",
+    image: "https://iili.io/JSyePNs.jpg",
   },
   {
     title: "How We Do It Right",
@@ -143,7 +141,6 @@ const dummyContent = [
       </>
     ),
     badge: "",
-    image:
-      "https://iili.io/JUdxrJ9.png",
+    image: "https://iili.io/JUdxrJ9.png",
   },
 ];
--- src/app/condition/page.tsx
+++ src/app/condition/page.tsx
@@ -3,22 +3,24 @@ import React from "react";
 export default function TermsAndConditions() {
   return (
     <>
-      <div className="mx-0 lg:mx-32">
+      <div className="mx-0 lg:mx-32 text-black dark:text-white">
         <div className="mt-20 flex justify-center items-center">
           <h1 className="text-2xl lg:text-4xl text-center text-yellow-500">
             Terms and Conditions
           </h1>
         </div>
         <div className="px-8 py-4">
-          <h3 className="text-lg underline font-semibold mb-4">Refund Policy:</h3>
+          <h3 className="text-lg underline font-semibold mb-4">
+            Refund Policy:
+          </h3>
           <p>There will be no refunds issued under any circumstances.</p>
         </div>
         <div className="px-8 py-4">
           <h3 className="text-lg underline font-semibold mb-4">Terms:</h3>
           <p>
             By accessing this website, you agree to be bound by these Terms and
-            Conditions of Use, as well as all applicable laws and regulations. If
-            you do not agree with any of these terms, you are prohibited from
+            Conditions of Use, as well as all applicable laws and regulations.
+            If you do not agree with any of these terms, you are prohibited from
             using or accessing this site. The materials on this website are
             protected by applicable copyright and trademark laws.
           </p>
@@ -26,80 +28,116 @@ export default function TermsAndConditions() {
         <div className="px-8 py-4">
           <h3 className="text-lg underline font-semibold mb-4">Use License:</h3>
           <p>
-            Permission is granted to temporarily download one copy of the materials
-            (information or software) on Fitnesspergram's website for personal,
-            non-commercial transitory viewing only. This is a license grant and not
-            a transfer of title. Under this license, you may not modify or copy the
-            materials, use them for any commercial purpose, attempt to decompile or
-            reverse engineer any software, or transfer the materials to another
-            person. This license will terminate automatically if you violate any of
-            these restrictions.
+            Permission is granted to temporarily download one copy of the
+            materials (information or software) on Fitnesspergram's website for
+            personal, non-commercial transitory viewing only. This is a license
+            grant and not a transfer of title. Under this license, you may not
+            modify or copy the materials, use them for any commercial purpose,
+            attempt to decompile or reverse engineer any software, or transfer
+            the materials to another person. This license will terminate
+            automatically if you violate any of these restrictions.
           </p>
         </div>
         <div className="px-8 py-4">
           <h3 className="text-lg underline font-semibold mb-4">Disclaimer:</h3>
           <p>
-            The materials on Fitnesspergram's website are provided "as is." Fitnesspergram makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.
+            The materials on Fitnesspergram's website are provided "as is."
+            Fitnesspergram makes no warranties, expressed or implied, and hereby
+            disclaims and negates all other warranties, including without
+            limitation, implied warranties or conditions of merchantability,
+            fitness for a particular purpose, or non-infringement of
+            intellectual property or other violation of rights.
           </p>
         </div>
         <div className="px-8 py-4">
           <h3 className="text-lg underline font-semibold mb-4">Limitations:</h3>
           <p>
-            In no event shall Fitnesspergram or its suppliers be liable for any damages arising out of the use or inability to use the materials on its website, even if Fitnesspergram has been notified of the possibility of such damage.
+            In no event shall Fitnesspergram or its suppliers be liable for any
+            damages arising out of the use or inability to use the materials on
+            its website, even if Fitnesspergram has been notified of the
+            possibility of such damage.
           </p>
         </div>
         <div className="px-8 py-4">
-          <h3 className="text-lg underline font-semibold mb-4">Delivery Instructions:</h3>
+          <h3 className="text-lg underline font-semibold mb-4">
+            Delivery Instructions:
+          </h3>
           <p>
-            Meals will be delivered together in the morning slot between 12:30 AM to 1:00 PM. Clients must be available to collect meals during this time. If no response is received from the client within 15-20 minutes, meals will not be compensated.
+            Meals will be delivered together in the morning slot between 12:30
+            AM to 1:00 PM. Clients must be available to collect meals during
+            this time. If no response is received from the client within 15-20
+            minutes, meals will not be compensated.
           </p>
         </div>
         <div className="px-8 py-4">
-          <h3 className="text-lg underline font-semibold mb-4">Cut-Off Policy:</h3>
+          <h3 className="text-lg underline font-semibold mb-4">
+            Cut-Off Policy:
+          </h3>
           <p>
-            Changes to meal selections or delivery addresses requested after 6 PM will not be guaranteed. Clients are advised to finalize meal choices and delivery details before this deadline.
+            Changes to meal selections or delivery addresses requested after 6
+            PM will not be guaranteed. Clients are advised to finalize meal
+            choices and delivery details before this deadline.
           </p>
         </div>
         <div className="px-8 py-4">
-          <h3 className="text-lg underline font-semibold mb-4">Guidelines for Contingencies:</h3>
+          <h3 className="text-lg underline font-semibold mb-4">
+            Guidelines for Contingencies:
+          </h3>
           <p>
-            Fitnesspergram will not compensate for delayed deliveries due to unforeseen events like heavy rainfall or natural calamities.
+            Fitnesspergram will not compensate for delayed deliveries due to
+            unforeseen events like heavy rainfall or natural calamities.
           </p>
         </div>
         <div className="px-8 py-4">
-          <h3 className="text-lg underline font-semibold mb-4">Revisions and Errata:</h3>
+          <h3 className="text-lg underline font-semibold mb-4">
+            Revisions and Errata:
+          </h3>
           <p>
-          Fitnesspergram does not warrant that the materials on its website are accurate, complete, or current.
+            Fitnesspergram does not warrant that the materials on its website
+            are accurate, complete, or current.
           </p>
         </div>
         <div className="px-8 py-4">
           <h3 className="text-lg underline font-semibold mb-4">Links:</h3>
           <p>
-          Fitnesspergram is not responsible for the contents of any linked sites. Use of any linked website is at the user's own risk.
+            Fitnesspergram is not responsible for the contents of any linked
+            sites. Use of any linked website is at the user's own risk.
           </p>
         </div>
         <div className="px-8 py-4">
-          <h3 className="text-lg underline font-semibold mb-4">Medical Conditions:</h3>
+          <h3 className="text-lg underline font-semibold mb-4">
+            Medical Conditions:
+          </h3>
           <p>
-          Fitnesspergram is not liable for any medical conditions experienced by clients during meal delivery. Clients should consult their medical practitioner before starting any diet plan.
+            Fitnesspergram is not liable for any medical conditions experienced
+            by clients during meal delivery. Clients should consult their
+            medical practitioner before starting any diet plan.
           </p>
         </div>
         <div className="px-8 py-4">
-          <h3 className="text-lg underline font-semibold mb-4">Code of Conduct Policy:</h3>
+          <h3 className="text-lg underline font-semibold mb-4">
+            Code of Conduct Policy:
+          </h3>
           <p>
-          Fitnesspergram reserves the right to terminate subscriptions if clients misbehave with company employees.
+            Fitnesspergram reserves the right to terminate subscriptions if
+            clients misbehave with company employees.
           </p>
         </div>
         <div className="px-8 py-4">
-          <h3 className="text-lg underline font-semibold mb-4">Governing Law:</h3>
+          <h3 className="text-lg underline font-semibold mb-4">
+            Governing Law:
+          </h3>
           <p>
-          Any claim relating to Fitnesspergram's website shall be governed by the laws of the State of Assam, India.
+            Any claim relating to Fitnesspergram's website shall be governed by
+            the laws of the State of Assam, India.
           </p>
         </div>
-        <h1 className="text-center text-sm lg:text-2xl text-rose-500 mt-5">By using this website, you agree to abide by these Terms and Conditions of Use.</h1>
+        <h1 className="text-center text-sm lg:text-2xl text-rose-500 mt-5">
+          By using this website, you agree to abide by these Terms and
+          Conditions of Use.
+        </h1>
       </div>
       <div className="mb-20"></div>
-
     </>
   );
 }
